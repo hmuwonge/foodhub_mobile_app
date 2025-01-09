ffe83150e4d5a7be4c0b12d687b917c8e0c226c8
Saava <hassansaava@gmail.com>
2025-01-09 19:34:14 +0300

    add padding

--- app/src/main/java/com/metadent/foodhub_android/MainActivity.kt
+++ app/src/main/java/com/metadent/foodhub_android/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -72,7 +73,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             FoodHubAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   AuthScreen()
+                    Box(modifier = Modifier.padding(innerPadding))
+                    {
+                        AuthScreen()
+                    }
+
                 }
             }
         }
