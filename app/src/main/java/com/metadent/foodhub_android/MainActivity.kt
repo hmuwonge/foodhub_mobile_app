d76e750c2e0615dd311d191b2435e4b0a9e93dd5
Saava <hassansaava@gmail.com>
2025-01-21 17:23:40 +0300

    added navigation animations

--- app/src/main/java/com/metadent/foodhub_android/MainActivity.kt
+++ app/src/main/java/com/metadent/foodhub_android/MainActivity.kt
@@ -8,6 +8,10 @@ import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -86,7 +90,31 @@ class MainActivity : ComponentActivity() {
                    val navController = rememberNavController()
                     NavHost(navController=navController,
                         startDestination = Auth,
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
+                        enterTransition = {
+                            slideIntoContainer(
+                                towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                                animationSpec = tween(300)
+                            )+ fadeIn(animationSpec = tween(300))
+                        },
+                        exitTransition = {
+                            slideOutOfContainer(
+                                towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                                animationSpec = tween(300)
+                            )+ fadeOut(animationSpec = tween(300))
+                        },
+                        popEnterTransition = {
+                            slideIntoContainer(
+                                towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                                animationSpec = tween(300)
+                            )+ fadeIn(animationSpec = tween(300))
+                        },
+                        popExitTransition = {
+                            slideOutOfContainer(
+                                towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                                animationSpec = tween(300)
+                            )+ fadeOut(animationSpec = tween(300))
+                        }
                     ){
                         composable<SignUp> {
                             SignUpScreen(navController)
@@ -96,7 +124,32 @@ class MainActivity : ComponentActivity() {
                             AuthScreen(navController)
                         }
 
-                        composable<Login> {
+                        composable<Login>(
+                            enterTransition = {
+                                slideIntoContainer(
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Up,
+                                    animationSpec = tween(300)
+                                )+ fadeIn(animationSpec = tween(300))
+                            },
+                            exitTransition = {
+                                slideOutOfContainer(
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Up,
+                                    animationSpec = tween(300)
+                                )+ fadeOut(animationSpec = tween(300))
+                            },
+                            popEnterTransition = {
+                                slideIntoContainer(
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Down,
+                                    animationSpec = tween(300)
+                                )+ fadeIn(animationSpec = tween(300))
+                            },
+                            popExitTransition = {
+                                slideOutOfContainer(
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Down,
+                                    animationSpec = tween(300)
+                                )+ fadeOut(animationSpec = tween(300))
+                            }
+                        ) {
                             Box(modifier = Modifier.fillMaxSize().background(Color.Green))
                         }
 
