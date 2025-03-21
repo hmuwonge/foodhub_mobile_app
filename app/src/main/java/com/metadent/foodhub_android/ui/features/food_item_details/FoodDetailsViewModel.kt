2d885d318d00a9fe3e3f32f456c3ff8baf68e2c1
Muwonge Hassan Saava <hassansaava@gmail.com>
2025-03-21 17:49:16 +0300

    fff

---
+++ app/src/main/java/com/metadent/foodhub_android/ui/features/food_item_details/FoodDetailsViewModel.kt
@@ -0,0 +1,9 @@
+package com.metadent.foodhub_android.ui.features.food_item_details
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FoodDetailsViewModel @Inject constructor(): ViewModel(){
+}
